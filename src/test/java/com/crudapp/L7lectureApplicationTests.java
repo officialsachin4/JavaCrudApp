99c7f6a1b5cd0dd9fe3facc61e09e80f04f03bf5
Sachin <officialsachinsharma@gmail.com>
2023-02-12 21:31:30 +0530

    Adding files to github

---
+++ src/test/java/com/crudapp/L7lectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L7lectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
